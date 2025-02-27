05e789a103b4c5202246b57953dc490418bf0134
Loup Thevenin <loup@mac.home>
2025-02-27 19:30:52 +0100

    redis OK

--- srcs/requirements/wordpress/conf/php.sh
+++ srcs/requirements/wordpress/conf/php.sh
@@ -10,4 +10,13 @@ sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
 sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
 cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
 
+# BONUS REDIS
+echo "extension=redis.so" >/etc/php/7.4/fpm/conf.d/redis.ini
+
+if ! grep -q "define('WP_REDIS_HOST', 'redis');" wp-config.php; then
+	echo "define('WP_REDIS_HOST', 'redis');" >>/var/www/wordpress/wp-config.php
+	echo "define('WP_REDIS_PORT', 6379);" >>/var/www/wordpress/wp-config.php
+	echo "define('WP_CACHE', true);" >>/var/www/wordpress/wp-config.php
+fi
+
 exec php-fpm7.4 -F
